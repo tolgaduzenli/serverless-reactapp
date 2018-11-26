87f25c04935b849e2caaef0b7a479e9935adc18a
tolgaduzenli <4467TLG339tlg>
2018-11-26 14:15:05 +0000

    Removed proxy url. Added api call utilty class.

--- src/service/APICall.js
+++ src/service/APICall.js
@@ -1,17 +1,22 @@
 import axios from 'axios'
+const API_URL = "https://7vsntirtje.execute-api.us-east-1.amazonaws.com/dev"
+const GET_URL = API_URL + '/notes'
+const POST_URL = API_URL + '/notes'
+const PUT_URL = API_URL + '/notes/'
+const DELETE_URL = API_URL + '/notes/'
 
 export const getAllNotesFromService = () => {
-    return axios.get('/notes')
+    return axios.get(GET_URL)
 }
 
 export const saveNewNote = (title, description) => {
-    return axios.post('/notes', { title, description })
+    return axios.post(POST_URL, { title, description })
 }
 
 export const updateNote = (id, title, description) => {
-    return axios.put('/notes/' + id, { title, description })
+    return axios.put(PUT_URL + id, { title, description })
 }
 
 export const deleteNote = (id) => {
-    return axios.delete('/notes/' + id)
+    return axios.delete(DELETE_URL + id)
 }
